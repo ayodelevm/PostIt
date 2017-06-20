70919773feeb5f4429d7dda5e3693e8e14f319d9
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-20 12:01:49 +0100

    refactoring login route and server responses

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,63 +5,70 @@ import User from '../models/user';
 
 const router = express.Router();
 
-
-router.get('/secret', (req, res) => {
-  res.render('secret');
-});
-
 // ======================
 // AUTH ROUTES
 // ======================
 
-// Show register form
-router.get('/register', (req, res) => {
-  res.render('register');
-});
-
 // Sign up logic
 router.post('/register', (req, res) => {
-  console.log(req.body);
-  // const newUser = new User({ username: req.body.username, fullname: req.body.fullname, email: req.body.email });
   User.register(req.body.username, req.body.password, (err, newUser) => {
 
     if (err) {
-      console.log(err);
       req.flash('error', `${err.message}`);
-      return res.render('register');
+      return res.status(500).json({
+        message: err.message
+      });
     }
     newUser.isAdmin = false;
     newUser.email = req.body.email;
-    newUser.fullName = req.body.fullname;
+    newUser.fullName = req.body.fullName;
     newUser.save();
 
     passport.authenticate('local')(req, res, () => {
-      console.log(req.dataValues);
       req.flash('success', 'Welcome to PostIt');
-      res.redirect('/secret');
+      res.status(200).json({
+        message: `Welcome to PostIt + ${req.session.passport.user}`,
+        user: req.session.passport.user
+      });
     });
   });
 });
 
-// Show login form
-router.get('/login', (req, res) => {
-  res.render('login');
-});
+// Login logic
 
-// Add login logic
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/secret',
-  failureRedirect: '/login',
-  failureFlash: true,
-  successFlash: true
-}), (req, res) => {
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (error, user, info) => {
+    if (error) {
+      return next(error);
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        error: info
+      });
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        return res.status(500).json({
+          message: 'Cannot log in user'
+        });
+      }
+      res.status(200).json({
+        message: `Welcome back ${req.session.passport.user}`,
+        user: req.session.passport.user
+      });
+    });
+  })(req, res, next);
 });
 
 // Add logout route
 router.get('/logout', (req, res) => {
   req.logout();
   req.flash('error', 'Loged out successful!');
-  res.redirect('/login');
+  res.status(200).json({
+    message: 'Logged out successfully'
+  });
 });
 
 export default router;

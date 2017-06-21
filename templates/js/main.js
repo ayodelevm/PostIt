7ca16c71e87a9e27d5a232688636b2651b3d3b16
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-21 16:51:07 +0100

    feature| adding html, css and js files for create-group template

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
     $('.slider').slider({ full_width: true });
+    $('.modal').modal();
 });
+
+// $(document).ready(function(){
+//     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+//     $('.modal').modal();
+//   });

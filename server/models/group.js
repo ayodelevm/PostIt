95403cac4eaf6ae6b2d2005c9e764dfc3ff748f7
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-24 02:53:09 +0100

    feature| updating models to include validations

--- server/models/group.js
+++ server/models/group.js
@@ -8,16 +8,36 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4
     },
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
     description: DataTypes.TEXT,
-    imageUrl: DataTypes.STRING
+    imageUrl: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    // groupMembersId: {
+    //   type: DataTypes.ARRAY(DataTypes.TEXT),
+    //   allowNull: false,
+    //   defaultValue: DataTypes.UUIDV4,
+    //   references: {
+    //     model: 'Users',
+    //     key: 'id',
+    //     deferrable: DataTypes.Deferrable.INITIALLY_DEFERRED
+    //   }
+    // }
   }, {
     classMethods: {
       associate: (models) => {
-        Group.hasMany(models.User);
+        // Group.hasMany(models.User);
         Group.hasMany(models.Message);
         Group.belongsTo(models.User, {
           foreignKey: 'UserId',
+          deferrable: DataTypes.Deferrable.INITIALLY_DEFERRED,
           onDelete: 'CASCADE'
         });
         // associations can be defined here
--- server/models/user.js
+++ server/models/user.js
@@ -8,20 +8,42 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4
     },
-    email: DataTypes.STRING,
-    password: DataTypes.TEXT,
-    username: DataTypes.STRING,
-    fullname: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        isApproved(value) {
+          const regExp = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+          if (!regExp.test(value)) {
+            throw new Error('Password must be a min of 8 letters with atleast a symbol, upper and lower case letters and a number');
+          }
+        }
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     mysalt: DataTypes.STRING
   }, {
     classMethods: {
       associate: (models) => {
         User.hasMany(models.Group);
         User.hasMany(models.Message);
-        User.belongsTo(models.Group, {
-          foreignKey: 'GroupId',
-          onDelete: 'CASCADE'
-        });
+        // User.belongsTo(models.Group, {
+        //   foreignKey: 'GroupId',
+        //   // deferrable: DataTypes.Deferrable.INITIALLY_DEFERRED,
+        //   onDelete: 'CASCADE'
+        // });
         // associations can be defined here
       }
     }

e5865c9b57b92c642f2cbfaeda8f799aee7738c9
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-27 20:43:03 +0100

    adding seeds file

---
+++ server/seeders/authSeeds.js
@@ -0,0 +1,24 @@
+export const user = [{
+  username: 'tundun',
+  password: 'tundun05',
+  email: 'tundun05@gmail.com',
+  fullname: 'tundun oluwalonimi'
+}, {
+  username: 'lolade',
+  password: 'lolade05',
+  email: 'lolade05@gmail.com',
+  fullname: 'lolade toluwanimi'
+}];
+
+export const loginUser = [{
+  username: 'lolade',
+  password: 'lolade05'
+}, {
+  username: 'lolade',
+  password: 'lolade'
+}];
+
+export const invalidUser = {
+  username: 'test',
+  password: 1234
+};
---
+++ server/seeders/groupSeeds.js
@@ -0,0 +1,15 @@
+export const groupDetails = [{
+  name: 'Learn C#',
+  description: 'We share in C#',
+  imageUrl: 'https://farm9.staticflickr.com/8076/8319742380_4c6dc9c3ef.jpg'
+}, {
+  name: 'Learn Python',
+  description: 'We speak the pythonic language',
+  imageUrl: 'https://farm9.staticflickr.com/8076/8319742380_4c6dc9c3ef.jpg'
+}];
+
+export const updateInfo = {
+  name: 'Learn Pythonic Python',
+  description: 'We speak the pythonic language',
+  imageUrl: 'https://farm9.staticflickr.com/8076/8319742380_4c6dc9c3ef.jpg'
+};

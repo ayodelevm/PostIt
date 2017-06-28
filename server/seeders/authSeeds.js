c53ea447feff0f6568d54871bd2d067aeda66f74
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-28 02:33:19 +0100

    adding seed files for testing

--- server/seeders/authSeeds.js
+++ server/seeders/authSeeds.js
@@ -8,15 +8,30 @@ export const user = [{
   password: 'lolade05',
   email: 'lolade05@gmail.com',
   fullname: 'lolade toluwanimi'
+}, {
+  username: 'bolu',
+  password: 'bolu05',
+  email: 'bolu05@yahoo.com',
+  fullname: 'bolu adebayo'
+}, {
+  username: 'jide',
+  password: 'jide05',
+  email: 'jide05@yahoo.com',
+  fullname: 'jide adebayo'
 }];
 
 export const loginUser = [{
   username: 'lolade',
   password: 'lolade05'
 }, {
+  username: 'jide',
+  password: 'jide05'
+}];
+
+export const incorrectPassword = {
   username: 'lolade',
   password: 'lolade'
-}];
+};
 
 export const invalidUser = {
   username: 'test',
--- server/seeders/groupSeeds.js
+++ server/seeders/groupSeeds.js
@@ -6,6 +6,10 @@ export const groupDetails = [{
   name: 'Learn Python',
   description: 'We speak the pythonic language',
   imageUrl: 'https://farm9.staticflickr.com/8076/8319742380_4c6dc9c3ef.jpg'
+}, {
+  name: 'Learn JavaScript',
+  description: 'We have that synthetic sugar you love',
+  imageUrl: 'https://farm9.staticflickr.com/8076/8319742380_4c6dc9c3ef.jpg'
 }];
 
 export const updateInfo = {

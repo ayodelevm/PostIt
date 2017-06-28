80a111b9acb8566e08b73e1391b1e684e2aeb6a6
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-06-28 02:32:48 +0100

    adding more tests for group and auth routes and adding tests for message routes

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -4,13 +4,13 @@ import 'chai';
 import should from 'should';
 import app from './../app';
 import models from '../models/index';
-import { user, loginUser, invalidUser } from './../seeders/authSeeds';
+import { user, loginUser, invalidUser, incorrectPassword } from './../seeders/authSeeds';
 
 const server = supertest.agent(app);
 
 
 before((done) => {
-  models.User.sync().then(() => {
+  models.sequelize.sync({ force: true }).then(() => {
     done(null);
   }).catch((errors) => {
     done(errors);
@@ -18,23 +18,25 @@ before((done) => {
 });
 
 describe('Authentication', () => {
-  afterEach((done) => {
-    models.User.sync({ force: true })
-      .then(() => {
-        done(null);
-      }).catch((errors) => {
-        done(errors);
-      });
-  });
-
   it('prevents accessing an undefined route', (done) => {
     server
     .get('/api/post')
     .expect(501)
     .end((err, res) => {
-      res.body.message.should.equal('Sorry, this route is not supported by this API.');
+      res.body.message.should.equal('Sorry, this address is not supported by this API.');
       done();
     });
+  });
+
+  it('prevents access to other routes if not logged in', (done) => {
+    server
+      .get('/api/group')
+      .expect(400)
+      .end((err, res) => {
+        res.status.should.equal(400);
+        res.body.message.should.equal('You need to be logged in to do that!');
+        done();
+      });
   });
 
   it('allows a new user to register', (done) => {
@@ -51,32 +53,8 @@ describe('Authentication', () => {
         done();
       });
   });
-});
 
-describe('Authentication', () => {
-  after((done) => {
-    models.User.sync({ force: true })
-      .then(() => {
-        done(null);
-      }).catch((errors) => {
-        done(errors);
-      });
-  });
-
-  it('prevents an invalid user from logging in', (done) => {
-    server
-      .post('/api/user/login')
-      .send(invalidUser)
-      .expect(404)
-      .end((err, res) => {
-        res.status.should.equal(401);
-        res.body.message.should.equal('User not found.');
-        res.body.error.message.should.equal('Incorrect username');
-        done();
-      });
-  });
-
-  it('allows a new user to create account and login later', (done) => {
+  it('allows a new user to register', (done) => {
     server
       .post('/api/user/register')
       .set('Connection', 'keep alive')
@@ -91,10 +69,70 @@ describe('Authentication', () => {
       });
   });
 
+  it('allows a new user to register', (done) => {
+    server
+      .post('/api/user/register')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(user[2])
+      .expect(200)
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.user.should.equal('bolu');
+        done();
+      });
+  });
+
+  it('allows a new user to register', (done) => {
+    server
+      .post('/api/user/register')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(user[3])
+      .expect(200)
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.user.should.equal('jide');
+        done();
+      });
+  });
+});
+
+describe('Authentication', () => {
+  it('prevents an invalid user from logging in', (done) => {
+    server
+      .post('/api/user/login')
+      .send(invalidUser)
+      .expect(404)
+      .end((err, res) => {
+        res.status.should.equal(401);
+        res.body.message.should.equal('User not found.');
+        res.body.error.message.should.equal('Incorrect username');
+        done();
+      });
+  });
+
+  it('should ensure that username is unique', (done) => {
+    server
+      .post('/api/user/register')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(user[1])
+      .expect(200)
+      .end((err, res) => {
+        res.status.should.equal(500);
+        res.body.error.should.equal('User already exists with lolade');
+        done();
+      });
+  });
+
   it('prevents a user with a wrong password from logging in', (done) => {
     server
       .post('/api/user/login')
-      .send(loginUser[1])
+      .send(incorrectPassword)
       .expect(501)
       .end((err, res) => {
         res.status.should.equal(401);
--- server/test/groups.test.js
+++ server/test/groups.test.js
@@ -3,45 +3,12 @@ import 'mocha';
 import 'chai';
 import should from 'should';
 import app from './../app';
-import models from '../models/index';
-import { user, loginUser } from './../seeders/authSeeds';
+import { loginUser } from './../seeders/authSeeds';
 import { groupDetails, updateInfo } from './../seeders/groupSeeds';
 
 const server = supertest.agent(app);
 
-before((done) => {
-  models.sequelize.sync({force:true}).then(() => {
-    done(null);
-  }).catch((errors) => {
-    done(errors);
-  });
-});
-
-after((done) => {
-  models.User.sync({ force: true })
-    .then(() => {
-      done(null);
-    }).catch((errors) => {
-      done(errors);
-    });
-});
-
 describe('Create Group Routes', () => {
-  it('allows a new user to create account and login later', (done) => {
-    server
-      .post('/api/user/register')
-      .set('Connection', 'keep alive')
-      .set('Content-Type', 'application/json')
-      .type('form')
-      .send(user[1])
-      .expect(200)
-      .end((err, res) => {
-        res.status.should.equal(200);
-        res.body.user.should.equal('lolade');
-        done();
-      });
-  });
-
   it('allows a registered user to login successfully', (done) => {
     server
     .post('/api/user/login')
@@ -67,7 +34,7 @@ describe('Create Group Routes', () => {
     .expect(200)
     .end((err, res) => {
       res.status.should.equal(201);
-      res.body.message.should.equal('New group created successfully.');
+      res.body.success.should.equal('New group created successfully.');
       done();
     });
   });
@@ -87,6 +54,21 @@ describe('Create Group Routes', () => {
     });
   });
 
+  it('allows a logged in user to create a new group', (done) => {
+    server
+    .post('/api/group')
+    .set('Connection', 'keep alive')
+    .set('Content-Type', 'application/json')
+    .type('form')
+    .send(groupDetails[2])
+    .expect(200)
+    .end((err, res) => {
+      res.status.should.equal(201);
+      res.body.success.should.equal('New group created successfully.');
+      done();
+    });
+  });
+
   it('allows a logged in user to get all the groups he belongs to', (done) => {
     server
     .get('/api/group')
@@ -94,12 +76,13 @@ describe('Create Group Routes', () => {
     .end((err, res) => {
       res.status.should.equal(200);
       res.body.success.should.equal('Successful.');
+      done();
     });
   });
 
   it('allows a group admin to edit the group details', (done) => {
     server
-    .post('/api/group/2/edit')
+    .put('/api/group/2/edit')
     .set('Connection', 'keep alive')
     .set('Content-Type', 'application/json')
     .type('form')
@@ -114,7 +97,7 @@ describe('Create Group Routes', () => {
 
   it('allows a group admin to delete the group he owns', (done) => {
     server
-    .get('/api/group/1/edit')
+    .delete('/api/group/1/delete')
     .expect(200)
     .end((err, res) => {
       res.status.should.equal(200);
@@ -123,6 +106,46 @@ describe('Create Group Routes', () => {
     });
   });
 
+  it('allows another registered user to login successfully', (done) => {
+    server
+    .post('/api/user/login')
+    .set('Connection', 'keep alive')
+    .set('Content-Type', 'application/json')
+    .type('form')
+    .send(loginUser[1])
+    .expect(200)
+    .end((err, res) => {
+      res.status.should.equal(200);
+      res.body.user.should.equal('jide');
+      done();
+    });
+  });
+
+  it('prevents a user from editing a group he is not a member of', (done) => {
+    server
+    .put('/api/group/2/edit')
+    .set('Connection', 'keep alive')
+    .set('Content-Type', 'application/json')
+    .type('form')
+    .send(updateInfo)
+    .expect(400)
+    .end((err, res) => {
+      res.status.should.equal(400);
+      res.body.error.should.equal('You are not authorized to access this group!');
+      done();
+    });
+  });
+
+  it('prevents a user from deleting a group he is not a member of', (done) => {
+    server
+    .delete('/api/group/1/delete')
+    .expect(400)
+    .end((err, res) => {
+      res.status.should.equal(400);
+      res.body.error.should.equal('You are not authorized to access this group!');
+      done();
+    });
+  });
 });
 
 

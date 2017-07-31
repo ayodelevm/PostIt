4a3931790c32d2374cd05187d97810aee5799fe8
ayodelevm <ayodelevm@outlook.com>
2017-07-31 13:14:49 +0100

    adding utility files containing methods for some http requests and file containving the different urls

---
+++ client/dev/js/utils/apis.js
@@ -0,0 +1,54 @@
+import 'isomorphic-fetch';
+import apiUrls from './apiUrls';
+
+
+export const checkStatus = (response) => {
+  if (response.ok) {
+    return response.json();
+  }
+  return response.json().then(Promise.reject.bind(Promise));
+};
+
+export const getEndpoint = (endpoint) => {
+  const url = `${apiUrls.BASE_PATH}${endpoint}`;
+  return fetch(url, {
+    credentials: 'include'
+  }).then(checkStatus);
+};
+
+export const postEndpoint = (endpoint, data) => {
+  // const url = `${apiUrls.BASE_PATH}${endpoint}`;
+  const url = `${'http://localhost:3002'}${endpoint}`;
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  }).then(checkStatus);
+};
+
+export const updateEndpoint = (endpoint, data) => {
+  const url = `${apiUrls.BASE_PATH}${endpoint}`;
+  return fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  }).then(checkStatus);
+};
+
+export const deleteEndpoint = (endpoint, data) => {
+  const url = `${apiUrls.BASE_PATH}${endpoint}`;
+  return fetch(url, {
+    method: 'DELETE',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  }).then(checkStatus);
+};

e34d3b99d0be7be7402c583b69f106cfdc51fa92
ayodelevm <ayodelevm@outlook.com>
2017-07-31 12:53:03 +0100

    updating webpack settings for reloading pages and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   devServer: {
+    historyApiFallback: true,
     inline: true,
     contentBase: path.join(__dirname, '/client/build'),
     port: 3000
@@ -19,14 +20,54 @@ module.exports = {
       {
         test: /\.scss/,
         loader: 'style-loader!css-loader!sass-loader'
-      }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      // {
+      //   test: /\.scss$/,
+      //   exclude: /node_modules/,
+      //   loader: 'style-loader!css-loader!sass-loader'
+      // },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|styl)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 250000,
+        },
+      },
     ]
   },
   output: {
     path: path.join(__dirname, '/client/build'),
-    filename: 'js/bundle.min.js'
+    filename: 'js/bundle.min.js',
+    publicPath: '/'
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Hammer: 'hammerjs/hammer'
+    }),
     new webpack.optimize.OccurrenceOrderPlugin()
   ]
 };

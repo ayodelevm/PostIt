2103cb8a52800e70f1d4128aadd36ca7ecac48dd
ayodelevm <ayodelevm@outlook.com>
2017-08-18 10:43:04 +0100

    refactored entry file to accomodate change in router and removing of commented code

--- client/dev/js/index.js
+++ client/dev/js/index.js
@@ -1,25 +1,25 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import createBrowserHistory from 'history/createBrowserHistory';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import promise from 'redux-promise';
+import firebase from 'firebase';
 
 import 'jquery/dist/jquery';
 import 'materialize-css/dist/css/materialize.css';
+import 'react-select/dist/react-select.css';
 import 'materialize-css/dist/js/materialize';
 import './js/main';
 
 import '../../../scss/main.scss';
 
-import store from './store/store';
+import store, { history } from './store/store';
 
 
 import App from './components/App';
-import Home from './components/Home';
-import Footer from './components/LandingFooter';
 import DashBoardContainer from './containers/DashBoardContainer';
+import MessagingContainer from './containers/MessagingContainer';
 import Landing from './components/Landing';
 import authorize from './utils/authorizeRoutes';
 
@@ -27,20 +27,32 @@ import authorize from './utils/authorizeRoutes';
 import LoginComponent from './components/LoginComponent';
 import SignUpComponent from './components/SignUpComponent';
 
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyBNWHCslAvK5NRxGh93Lw90Og5e5TR9BC0',
+  authDomain: 'postit-11cbe.firebaseapp.com',
+  databaseURL: 'https://postit-11cbe.firebaseio.com',
+  projectId: 'postit-11cbe',
+  storageBucket: '',
+  messagingSenderId: '100413825842'
+};
+firebase.initializeApp(config);
+
 
 ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <App>
           <Switch>
             <Route exact path="/" forceRefresh component={Landing} />
             <Route exact path="/register" component={SignUpComponent} />
             <Route exact path="/login" component={LoginComponent} />
             <Route path="/dashboard" component={authorize(DashBoardContainer)} />
+            <Route path="/groups/:id/message" component={authorize(MessagingContainer)} />
           </Switch>
-          {/* <Route path="/home" component={Home} /> */}
         </App>
-      </BrowserRouter>
+      </ConnectedRouter>
     </Provider>,
   document.getElementById('root')
 );
+

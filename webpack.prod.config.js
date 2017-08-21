5fd6831fe3542ae38dcba799dca1495426dbd458
ayodelevm <ayodelevm@outlook.com>
2017-08-21 19:34:06 +0100

    setting up webpack for production and serving react file from app.js file

---
+++ webpack.prod.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = [{
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/dev/js/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, '/client/build'),
+    publicPath: '/',
+    filename: 'js/bundle.min.js',
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        query: {
+          presets: ['es2015', 'react']
+        },
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|styl)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 250000,
+        },
+      },
+    //   {
+    //     test: /\.js$/,
+    //     include: [
+    //       path.join(__dirname, 'client'),
+    //       path.join(__dirname, 'server/')
+    //     ]
+    //   }
+    ]
+  }
+}];
+
+module.exports = config;

754dddc4eff23f3eb68d3e8b30998d6964b012c8
ayodelevm <ayodelevm@outlook.com>
2017-08-30 23:47:16 +0100

    chore(test): change test implementation from session to token, updated seed data and refactored status code

--- server/test/3-messages.test.js
+++ server/test/3-messages.test.js
@@ -1,7 +1,6 @@
 import supertest from 'supertest';
-import 'mocha';
-import 'chai';
 import should from 'should';
+import jwtDecode from 'jwt-decode';
 import app from './../app';
 import { loginUser } from './../seeders/authSeeds';
 import message from './../seeders/messageSeeds';
@@ -9,24 +8,26 @@ import message from './../seeders/messageSeeds';
 const server = supertest.agent(app);
 
 describe('Messages Routes', () => {
+  let token;
+
   it('allows a registered user to login successfully', (done) => {
     server
-    .post('/api/user/login')
-    .set('Connection', 'keep alive')
-    .set('Content-Type', 'application/json')
-    .type('form')
+    .post('/api/v1/user/login')
     .send(loginUser[0])
     .expect(200)
     .end((err, res) => {
+      token = res.body.token;
+      const decodedUser = jwtDecode(res.body.token);
       res.status.should.equal(200);
-      res.body.user.should.equal('lolade');
+      decodedUser.username.should.equal('lolade');
       done();
     });
   });
 
   it('allows a group member to get one group and it\'s messages', (done) => {
     server
-    .get('/api/group/2/messages')
+    .get('/api/v1/group/2/messages')
+    .set('Authorization', `Bearer ${token}`)
     .expect(200)
     .end((err, res) => {
       res.status.should.equal(200);
@@ -37,10 +38,8 @@ describe('Messages Routes', () => {
 
   it('allows a group member to post messages in group he belongs', (done) => {
     server
-    .post('/api/group/2/message')
-    .set('Connection', 'keep alive')
-    .set('Content-Type', 'application/json')
-    .type('form')
+    .post('/api/v1/group/2/message')
+    .set('Authorization', `Bearer ${token}`)
     .send(message[0])
     .expect(201)
     .end((err, res) => {
@@ -52,10 +51,8 @@ describe('Messages Routes', () => {
 
   it('allows a group member to post messages in group he belongs', (done) => {
     server
-    .post('/api/group/3/message')
-    .set('Connection', 'keep alive')
-    .set('Content-Type', 'application/json')
-    .type('form')
+    .post('/api/v1/group/3/message')
+    .set('Authorization', `Bearer ${token}`)
     .send(message[0])
     .expect(201)
     .end((err, res) => {
@@ -67,10 +64,8 @@ describe('Messages Routes', () => {
 
   it('should ensure that messages are not empty', (done) => {
     server
-    .post('/api/group/3/message')
-    .set('Connection', 'keep alive')
-    .set('Content-Type', 'application/json')
-    .type('form')
+    .post('/api/v1/group/3/message')
+    .set('Authorization', `Bearer ${token}`)
     .send({ message: null })
     .expect(400)
     .end((err, res) => {

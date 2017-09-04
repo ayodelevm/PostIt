b837c434b0f294e02921dc0cc66bbcfeab171dfe
ayodelevm <ayodelevm@outlook.com>
2017-09-04 08:43:12 +0100

    feat(google-signin): add the presentational comnent for googe signin and signup and refactored input forms into a reusable component

--- client/dev/js/components/LoginComponent.jsx
+++ client/dev/js/components/LoginComponent.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LoginForm from '../containers/LoginForm.jsx';
+import GoogleLogin from 'react-google-login';
+import Notifications from 'react-notify-toast';
 import ResetPasswordEmailContainer from '../containers/ResetPasswordEmailContainer.jsx';
+import InputFieldGroup from './common/InputFields.jsx';
+
 
 /**
  * Gives the presentational view for the users login component
  * @param {object} props
  * @returns {void}
  */
-const LoginComponent = () => (
+const LoginComponent = (props) => (
   <div className="parallax-container">
     <div className="container register center-align">
       <div className="row">
@@ -29,7 +32,51 @@ const LoginComponent = () => (
               <div className="row form-section">
 
                 <p className="left">Required fields are marked *</p>
-                <LoginForm />
+
+                <form className="col s12" onSubmit={props.onSubmit} noValidate>
+                  <div className="main">
+                    <Notifications />
+                  </div>
+                  <div className="divider" />
+                  <br />
+
+                  <div className="row">
+                    <InputFieldGroup
+                      name={'userIdentifier'}
+                      placeholder={'vic ayo'}
+                      id={'userIdentifier'}
+                      value={props.state.userIdentifier}
+                      label={'Username or E-mail *'}
+                      error={props.state.errors.userIdentifier}
+                      type={'text'}
+                      onChange={props.onChange}
+                      htmlFor={'userIdentifier'}
+                    />
+                    <span className="left error-message grey-text">{props.state.errors.userIdentifier}</span>
+                  </div>
+                  <div className="row">
+                    <InputFieldGroup
+                      name={'password'}
+                      placeholder={'Your Password'}
+                      id={'password'}
+                      value={props.state.password}
+                      label={'Password *'}
+                      error={props.state.errors.password}
+                      type={'password'}
+                      onChange={props.onChange}
+                      htmlFor={'password'}
+                    />
+                    <span className="left error-message grey-text">{props.state.errors.password}</span>
+                  </div>
+
+                  <div className="row">
+                    <div className="input-field col s12">
+                      <button className="btn lime accent-4 waves-effect waves-light center" type="submit" name="action">Login
+                        <i className="material-icons right">person</i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
               </div>
               <div className="row">
                 <p className="center">Forgot password?
@@ -40,8 +87,19 @@ const LoginComponent = () => (
 
             </div>
             <div className="card-action">
-              <span className="black-text">New to PostIt? </span>
-              <a href="/register" className="green-text darken-4">Create a new account</a>
+              <div className="card-action-text">
+                <span className="black-text">New to PostIt? </span>
+                <a href="/register" className="green-text darken-4">Create a new account</a>
+              </div><br />
+              <span className="black-text"> or Login with </span>
+              <GoogleLogin
+                clientId={props.clientId}
+                onSuccess={props.onSuccess}
+                onFailure={props.onFailure}
+                className={'google-login'}
+              >
+                <a className="waves-effect waves-light white-text btn red">Google+</a>
+              </GoogleLogin>
             </div>
           </div>
         </div>
